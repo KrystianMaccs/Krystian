ae2864e855b0b3c8b85843914498c1e678c1dce0
Krystian Maccs <krystianmaccs@gmail.com>
2023-03-24 23:44:40 +0100

    chore: modified resume

--- src/components/Resume/ResumeNew.jsx
+++ src/components/Resume/ResumeNew.jsx
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/Christian_Maccs_CV.pdf";
+import pdf from "../../Assets/../Assets/Christian_Maximilian_CV.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+  "https://github.com/KrystianMaccs/Krystian/blob/b983f4bb7c469774d1f4c3289d93fbc850a4f21d/src/Assets/Christian_Maximilian_CV.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
@@ -17,7 +17,6 @@ function ResumeNew() {
   useEffect(() => {
     setWidth(window.innerWidth);
   }, []);
-
   return (
     <div>
       <Container fluid className="resume-section">
